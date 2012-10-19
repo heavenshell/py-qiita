8f0d97d7342d610cd215dd6695a6e5c32d646b2c
heavenshell <heavenshell.jp@gmail.com>
2012-10-20 03:30:23 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+    qiita
+    ~~~~~
+
+    Qiita api wrapper for Python.
+
+    :copyright: (c) 2012 Shinya Ohyanagi, All rights reserved.
+    :license: BSD, see LICENSE for more details.
+"""
+import os
+from setuptools import setup, find_packages
+
+app_name = 'qiita'
+
+description = file(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
+setup(
+    name=app_name,
+    version='0.1',
+    author='Shinya Ohyanagi',
+    author_email='sohyanagi@gmail.com',
+    url='http://github.com/heavenshell/py-qiita',
+    description='Qiita api wrapper for Python',
+    long_description=description,
+    license='BSD',
+    platforms='any',
+    packages=find_packages(app_name),
+    package_dir={'': app_name},
+    install_requires=[
+        'requests',
+        'mock'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+    ],
+    tests_require=['mock>=1.0.0'],
+    test_suite='tests'
+)
